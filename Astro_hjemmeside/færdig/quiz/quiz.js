492661c7620deb7b9516a5279c2ca54edd5b635f
AndreasErslev <57252859+AndreasErslev@users.noreply.github.com>
2021-10-10 11:58:28 +0200

    Slet af hjemmesider | forkert mappe
    Ved et uheld kom filerne i forkert mappe

--- Astro_hjemmeside/færdig/quiz/quiz.js
+++
@@ -1,6 +0,0 @@
- $(document).ready(function() {
-    $('label').click(function() {
-        $('label').removeClass('worngans');
-        $(this).addClass('worngans');
-    });
-});
--- kokaergaard/assets/js/pagescroll.js
+++
@@ -1,4 +0,0 @@
-	function Scroll() {
-	var elmnt = document.getElementById("Scroll");
-	elmnt.scrollIntoView({ behavior: 'smooth', block: 'start'});
-	}
